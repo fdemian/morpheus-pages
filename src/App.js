93c36cc666c8251da504e1e4300a34df35f77e23
fdemian <federicodamiancaminiti@gmail.com>
2020-05-19 18:13:26 -0300

    Customize website.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,98 @@
+/* eslint no-undef: 0 */
+/* eslint arrow-parens: 0 */
 import React from 'react';
-import logo from './logo.svg';
+import { enquireScreen } from 'enquire-js';
+
+import Navbar from './Navbar/Navbar';
+import ContentHeader from './ContentHeader/ContentHeader';
+import Feature7 from './Feature7/Feature7';
+import Content5 from './Content5/Content5';
+import Content3 from './Content3/Content3';
+//import Footer1 from './Footer1';
+
+import NavbarDS from './Navbar/datasource';
+import ContentHeaderDS from './ContentHeader/datasource';
+import Feature7DS from './Feature7/datasource';
+import Content5DS from './Content5/datasource';
+import Content3DS from './Content3/datasource';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+let isMobile;
+enquireScreen((b) => { isMobile = b; });
 
-export default App;
+const { location } = window;
+
+export default class Home extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMobile,
+      show: !location.port
+    };
+  }
+
+  componentDidMount() {
+    enquireScreen((b) => {
+      this.setState({ isMobile: !!b });
+    });
+
+    if (location.port) {
+
+      setTimeout(() => {
+        this.setState({
+          show: true,
+        });
+      }, 500);
+    }
+
+  }
+
+  render() {
+
+    const children = [
+      <Navbar
+        id="Nav0_0"
+        key="Nav0_0"
+        dataSource={NavbarDS}
+        isMobile={this.state.isMobile}
+      />,
+      <ContentHeader
+        id="Content13_0"
+        key="Content13_0"
+        dataSource={ContentHeaderDS}
+        isMobile={this.state.isMobile}
+      />,
+      <Feature7
+        id="Feature7_0"
+        key="Feature7_0"
+        dataSource={Feature7DS}
+        isMobile={this.state.isMobile}
+      />,
+      <Content5
+        id="Content5_0"
+        key="Content5_0"
+        dataSource={Content5DS}
+        isMobile={this.state.isMobile}
+      />,
+      <Content3
+        id="Content3_0"
+        key="Content3_0"
+        dataSource={Content3DS}
+        isMobile={this.state.isMobile}
+      />
+    ];
+
+    return (
+      <div
+        className="templates-wrapper"
+        ref={(d) => {
+          this.dom = d;
+        }}
+      >
+        {this.state.show && children}
+      </div>
+    );
+  }
+}
---
+++ src/ContentHeader/datasource.js
@@ -0,0 +1,30 @@
+ const Content13 = {
+  OverPack: {
+    className: 'home-page-wrapper content13-wrapper',
+    playScale: 0.3,
+  },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'image',
+        children:
+          'https://gw.alipayobjects.com/zos/rmsportal/PiqyziYmvbgAudYfhuBr.svg',
+        className: 'title-image',
+      },
+      { name: 'title', children: 'Morpheus', className: 'title-h1' },
+      {
+        name: 'content',
+        children: 'A better way to communicate.',
+        className: 'title-content',
+      },
+      {
+        name: 'content2',
+        children: 'Morpheus is a self-hosted alternative to third-party hosted blogging.',
+        className: 'title-content',
+      },
+    ],
+  },
+};
+
+export default Content13;
